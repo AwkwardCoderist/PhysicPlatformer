c22b2fde6d4bc0e303970e951282b9816e463a09
r-elizarov <mineplay.rommax@gmail.com>
2023-06-05 00:05:12 +0300

    Апдейт перед билдом

--- Assets/MainProject/Scripts/FixPSDImporter.cs
+++ Assets/MainProject/Scripts/FixPSDImporter.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class FixPSDImporter
-{
+{/*
     [UnityEditor.InitializeOnLoadMethod]
     public static void ResetPSDImporterFoldout()
     {
         UnityEditor.EditorPrefs.DeleteKey("PSDImporterEditor.m_PlatformSettingsFoldout");
     }
-    //
+    */
 }
